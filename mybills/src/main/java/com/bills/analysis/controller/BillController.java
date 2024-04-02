df3da262e5c7448ea4526fb68e2005bf1cddd3a7
omeraltan <omeraltan66@gmail.com>
2024-04-02 11:11:06 +0300

    Commit All

--- mybills/src/main/java/com/bills/analysis/controller/BillController.java
+++ mybills/src/main/java/com/bills/analysis/controller/BillController.java
@@ -1,4 +1,17 @@
 package com.bills.analysis.controller;
 
+import io.swagger.annotations.ApiOperation;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class BillController {
+
+    @GetMapping("/fatura")
+    @ApiOperation(value = "Fatura Menu Method")
+    public String getFatura(Model model){
+        return "fatura";
+    }
+
 }
--- mybills/src/main/java/com/bills/analysis/service/InstitutionService.java
+++ mybills/src/main/java/com/bills/analysis/service/InstitutionService.java
@@ -1,4 +1,13 @@
 package com.bills.analysis.service;
 
+import com.bills.analysis.entity.Institution;
+
+import java.util.List;
+
 public interface InstitutionService {
+
+    public List<Institution> getAllInstitution();
+    public void saveInstitution(Institution institution);
+
+
 }
